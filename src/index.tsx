ef7dfda31bab46269d87cd2f6ff871f1dcdc3139
MicroBlock <66859419+MicroCBer@users.noreply.github.com>
2023-01-16 16:36:19 +0800

    removed unused code

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,6 @@ import * as React from "react";
 import { MantineProvider, createStyles } from "@mantine/core";
 import { getConfig, getFullConfig } from "./config/core";
 
-export let cssContent = "";
-
-const camelToSnakeCase = (str: string) =>
-	str.replace(/[A-Z]/g, (letter) => `-${letter.toLowerCase()}`);
-
 
 interface LNAPINCMPlugin extends NCMPlugin {
 	isRequestAvailable(): boolean;
@@ -31,15 +26,6 @@ plugin.onLoad(function () {
 	if (DEBUG) {
 		const debounceReload = betterncm.utils.debounce(betterncm.reload, 1000);
 
-		const debounceRefreshStyle = function () {
-			const curStyle = betterncm_native.fs.readFileText(
-				`${plugin.pluginPath}/index.css`,
-			);
-			if (cssContent !== curStyle) {
-				cssContent = curStyle;
-				reloadStylesheet(cssContent);
-			}
-		};
 
 		const shouldReloadPaths = ["/manifest.json", "/index.js"];
 
@@ -66,10 +52,7 @@ plugin.onLoad(function () {
 			);
 			if (currentOriginalFiles[relPath] !== fileData) {
 				currentOriginalFiles[relPath] = fileData;
-				if (relPath === "/index.css") {
-					warn("检测到", relPath, "更新，正在热重载样式表");
-					debounceRefreshStyle();
-				} else if (shouldReloadPaths.includes(relPath)) {
+				if (shouldReloadPaths.includes(relPath)) {
 					warn(
 						"检测到",
 						relPath,
@@ -100,14 +83,6 @@ plugin.onLoad(function () {
 			},
 		);
 	}
-	betterncm.fs
-		.readFileText(`${plugin.pluginPath}/index.css`)
-		.then((curStyle) => {
-			if (cssContent !== curStyle) {
-				cssContent = curStyle;
-				reloadStylesheet(cssContent);
-			}
-		});
 });
 
 plugin.onConfig(()=>{
@@ -123,40 +98,6 @@ plugin.onConfig(()=>{
 
 	return root;
 })
-
-// window.addEventListener(
-// 	"load",
-// 	() => {
-// 		// 把所有被 Corona 遥测过的函数还原
-// 		for (const key in window) {
-// 			if (typeof window[key] === "function") {
-// 				if ("__corona__" in window[key] && "__orig__" in window[key]) {
-// 					// rome-ignore lint/suspicious/noExplicitAny: <explanation>
-// 					window[key] = (window[key] as any).__orig__;
-// 					log("已还原被遥测函数", `window.${key}`);
-// 				}
-// 			}
-// 		}
-// 	},
-// 	{
-// 		once: true,
-// 	},
-// );
-
-if (OPEN_PAGE_DIRECTLY) {
-	window.addEventListener(
-		"load",
-		() => {
-			const btn = document.querySelector<HTMLAnchorElement>(
-				"a[data-action='max']",
-			);
-			btn?.click();
-		},
-		{
-			once: true,
-		},
-	);
-}
 
 
 
